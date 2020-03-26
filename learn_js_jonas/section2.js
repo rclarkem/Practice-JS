872395e8914af88e71e1e9e52d3b866689ef1add
Reina <r.clarkemitchell@gmail.com>
2020-03-26 01:28:23 -0400

    first commit

---
+++ learn_js_jonas/section2.js
@@ -0,0 +1,50 @@
+// Practice JS
+
+// Variables
+
+// strings
+let firstName = 'Reina'
+// console.log(firstName)
+let lastName = 'Mitchell'
+// console.log(lastName)
+
+// int
+let age = 26
+// console.log(age)
+
+// boolean
+let fullAge = true
+// console.log(fullAge)
+
+/**
+ Variable mutation and type coercion
+ */
+
+console.log(firstName + ' ' + age)
+let job = 'teacher',
+	isMarried = false
+// console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is she married? ' + isMarried)
+
+age = 'twenty-eight'
+job = 'driver'
+// console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is she married? ' + isMarried)
+
+// lastName = prompt('What is her last name? ')
+// console.log(lastName)
+
+// Math Operators
+
+let year = 2020
+let yearReinaBorn = year - 1993
+let yearMarkBorn = year - 1983
+
+console.log(yearReinaBorn)
+console.log(yearMarkBorn)
+
+let ageReina = 27
+let ageMark = 37
+
+// Logical operators
+
+let markOlder = ageMark > ageReina
+console.log(markOlder)
