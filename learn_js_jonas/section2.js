e50a7c123510c64f04ab5ba5817e020a1f53197d
Reina <r.clarkemitchell@gmail.com>
2020-08-21 18:01:35 -0400

    change file structure and added new folders for application website.

--- learn_js_jonas/section2.js
+++ learn_js_jonas/section2.js
@@ -1,19 +1,21 @@
+/** @format */
+
 // Practice JS
 
 // Variables
 
 // strings
-let firstName = 'Reina'
+let firstName = "Reina";
 // console.log(firstName)
-let lastName = 'Mitchell'
+let lastName = "Mitchell";
 // console.log(lastName)
 
 // int
-let age = 26
+let age = 26;
 // console.log(age)
 
 // boolean
-let fullAge = true
+let fullAge = true;
 // console.log(fullAge)
 
 /**
@@ -22,13 +24,13 @@ let fullAge = true
 
 // console.log(firstName + ' ' + age)
 
-let job, isMarried
-job = 'teacher'
-isMarried = false
+let job, isMarried;
+job = "teacher";
+isMarried = false;
 // console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is she married? ' + isMarried)
 
-age = 'twenty-eight'
-job = 'driver'
+age = "twenty-eight";
+job = "driver";
 // console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is she married? ' + isMarried)
 
 // lastName = prompt('What is her last name? ')
@@ -36,18 +38,18 @@ job = 'driver'
 
 // Math Operators
 
-let year = 2020
-let yearReinaBorn = year - 1993
-let yearMarkBorn = year - 1983
+let year = 2020;
+let yearReinaBorn = year - 1993;
+let yearMarkBorn = year - 1983;
 
 // console.log(yearReinaBorn)
 // console.log(yearMarkBorn)
 
-let ageReina = 27
-let ageMark = 37
+let ageReina = 27;
+let ageMark = 37;
 
 // Logical operators
-let markOlder = ageMark > ageReina
+let markOlder = ageMark > ageReina;
 // console.log(markOlder)
 
 // typeof operator
@@ -56,26 +58,28 @@ let markOlder = ageMark > ageReina
 // console.log(typeof 'Reina is younger than Mark.')
 
 // Operator Precedence
-let ofAge = 18
-let isFullAge = year - 1983 >= ofAge
+let ofAge = 18;
+let isFullAge = year - 1983 >= ofAge;
 // console.log(isFullAge)
 
-let x, y
+let x, y;
 
-x = (3 + 5) * 4 - 6 // 8 * 4 - 6 => // 32 - 6 => // 26
+x = (3 + 5) * 4 - 6; // 8 * 4 - 6 => // 32 - 6 => // 26
 // console.log(x)
 
 // Code Challenge 1
 
-let markWeight = 68
-let markHeight = 1.82
+let markWeight = 68;
+let markHeight = 1.82;
 
-let johnWeight = 81
-let johnHeight = 1.87
+let johnWeight = 81;
+let johnHeight = 1.87;
 
-let markBMI = markWeight / markHeight ** 2
-let johnBMI = johnWeight / johnHeight ** 2
+let markBMI = markWeight / markHeight ** 2;
+let johnBMI = johnWeight / johnHeight ** 2;
 
-let bool = johnBMI >= markBMI
+let bool = johnBMI >= markBMI;
 // console.log("Is John's BMI greater than or equal to Mark's? " + bool)
 // console.log(markBMI, johnBMI)
+
+
