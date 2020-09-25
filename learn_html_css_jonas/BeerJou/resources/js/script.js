a337db0d13e68662e1ff21bfdf4f1a6c50e799c9
Reina <r.clarkemitchell@gmail.com>
2020-09-25 10:36:12 -0400

    responsive navbar

--- learn_html_css_jonas/BeerJou/resources/js/script.js
+++ learn_html_css_jonas/BeerJou/resources/js/script.js
@@ -50,39 +50,14 @@ $(document).ready(function () {
 
     nav.slideToggle(200);
 
-    if (icon.hasClass('menu-outline')) {
-      icon.addClass('ion-close-round');
-      icon.removeClass('menu-outline');
+    if (icon.attr('name') === 'menu-outline') {
+      icon.attr('name', 'close-outline');
+      // icon.removeClass('menu-outline');
     } else {
-      icon.addClass('menu-outline');
-      icon.removeClass('ion-close-round');
+      icon.attr('name', 'menu-outline');
+      // icon.atrr('ion-close-round');
     }
   });
-
-  // $('.js--nav-icon, .js--main-nav a, .logo-black').click(function (element) {
-  //   var nav = $('.js--main-nav');
-  //   var icon = $('.js--nav-icon ion-icon');
-
-  //   //Gets the class name of the element that triggered the event
-  //   var clicked = element.target.className;
-
-  //   //Exists the function if the menu is closed AND the logo-black element (logo image) was clicked
-  //   if (icon.hasClass('menu-outline') && clicked == 'logo-black') return;
-
-  //   //Opens and closes the menu
-  //   if ($(window).width() < 768) {
-  //     nav.slideToggle(200);
-  //   }
-
-  //   //Changes icon states of the menu button
-  //   if (icon.hasClass('menu-outline')) {
-  //     icon.addClass('ion-close-round');
-  //     icon.removeClass('menu-outline');
-  //   } else {
-  //     icon.addClass('menu-outline');
-  //     icon.removeClass('ion-close-round');
-  //   }
-  // });
 
   $(window).resize(function () {
     var nav = $('.js--main-nav');
@@ -90,32 +65,12 @@ $(document).ready(function () {
 
     if ($(window).width() > 767) {
       nav.css('display', 'block');
-      icon.addClass('ion-close-round');
-      icon.removeClass('menu-outline');
+      icon.attr('name', 'close-outline');
+      // icon.removeClass('menu-outline');
     } else {
       nav.css('display', 'none');
-      icon.addClass('menu-outline');
-      icon.removeClass('ion-close-round');
+      icon.attr('name', 'menu-outline');
+      // icon.removeClass('ion-close-round');
     }
   });
-
-  // $(window).resize(function () {
-  //   var nav = $('.js--main-nav');
-
-  //   var icon = $('.js--nav-icon i');
-
-  //   if ($(window).width() > 767) {
-  //     nav.css('display', 'block');
-
-  //     icon.addClass('ion-close-round');
-
-  //     icon.removeClass('ion-navicon-round');
-  //   } else {
-  //     nav.css('display', 'none');
-
-  //     icon.addClass('ion-navicon-round');
-
-  //     icon.removeClass('ion-close-round');
-  //   }
-  // });
 });
