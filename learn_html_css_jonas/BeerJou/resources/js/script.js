3e90e70cd4f38ad63d8ed492b0dec37691d315c9
Reina <r.clarkemitchell@gmail.com>
2020-09-25 01:54:32 -0400

    skeleton for responsive navbar.

--- learn_html_css_jonas/BeerJou/resources/js/script.js
+++ learn_html_css_jonas/BeerJou/resources/js/script.js
@@ -44,6 +44,61 @@ $(document).ready(function () {
     }
   );
 
+  $('.js--nav-icon').click(function () {
+    var nav = $('.js--main-nav');
+    var icon = $('.js--nav-icon ion-icon');
+
+    nav.slideToggle(200);
+
+    if (icon.hasClass('menu-outline')) {
+      icon.addClass('ion-close-round');
+      icon.removeClass('menu-outline');
+    } else {
+      icon.addClass('menu-outline');
+      icon.removeClass('ion-close-round');
+    }
+  });
+
+  // $('.js--nav-icon, .js--main-nav a, .logo-black').click(function (element) {
+  //   var nav = $('.js--main-nav');
+  //   var icon = $('.js--nav-icon ion-icon');
+
+  //   //Gets the class name of the element that triggered the event
+  //   var clicked = element.target.className;
+
+  //   //Exists the function if the menu is closed AND the logo-black element (logo image) was clicked
+  //   if (icon.hasClass('menu-outline') && clicked == 'logo-black') return;
+
+  //   //Opens and closes the menu
+  //   if ($(window).width() < 768) {
+  //     nav.slideToggle(200);
+  //   }
+
+  //   //Changes icon states of the menu button
+  //   if (icon.hasClass('menu-outline')) {
+  //     icon.addClass('ion-close-round');
+  //     icon.removeClass('menu-outline');
+  //   } else {
+  //     icon.addClass('menu-outline');
+  //     icon.removeClass('ion-close-round');
+  //   }
+  // });
+
+  $(window).resize(function () {
+    var nav = $('.js--main-nav');
+    var icon = $('.js--nav-icon ion-icon');
+
+    if ($(window).width() > 767) {
+      nav.css('display', 'block');
+      icon.addClass('ion-close-round');
+      icon.removeClass('menu-outline');
+    } else {
+      nav.css('display', 'none');
+      icon.addClass('menu-outline');
+      icon.removeClass('ion-close-round');
+    }
+  });
+
   // $(window).resize(function () {
   //   var nav = $('.js--main-nav');
 
