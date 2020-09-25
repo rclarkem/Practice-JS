88f3ffb4c94efc2cfe8c466627a0db52dbe36110
Reina <r.clarkemitchell@gmail.com>
2020-09-24 22:47:16 -0400

    animations for the app description

--- learn_html_css_jonas/BeerJou/resources/js/script.js
+++ learn_html_css_jonas/BeerJou/resources/js/script.js
@@ -24,4 +24,35 @@ $(document).ready(function () {
   $('.js--scroll-to-iphone').click(function () {
     $('html, body').animate({ scrollTop: $('#iphone').offset().top }, 1000);
   });
+
+  // Animations
+
+  $('.js--wp-1').waypoint(
+    function (direction) {
+      $('.js--wp-1').addClass('animate__fadeIn');
+    },
+    {
+      offset: '50%',
+    }
+  );
+
+  // $(window).resize(function () {
+  //   var nav = $('.js--main-nav');
+
+  //   var icon = $('.js--nav-icon i');
+
+  //   if ($(window).width() > 767) {
+  //     nav.css('display', 'block');
+
+  //     icon.addClass('ion-close-round');
+
+  //     icon.removeClass('ion-navicon-round');
+  //   } else {
+  //     nav.css('display', 'none');
+
+  //     icon.addClass('ion-navicon-round');
+
+  //     icon.removeClass('ion-close-round');
+  //   }
+  // });
 });
